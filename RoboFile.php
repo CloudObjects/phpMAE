f8b0f00a37f9f3fd9a6d105d9c3940cef8ceae26
Lukas Rosenstock <lr@lukasrosenstock.net>
2017-09-01 17:54:32 +0200

    Updated function whitelist with systematically generated list

--- RoboFile.php
+++ RoboFile.php
@@ -49,6 +49,10 @@ class RoboFile extends \Robo\Tasks {
             ? json_decode(file_get_contents('data/function.blacklist.json'), true) : [];
         
         $client = new \GuzzleHttp\Client([ 'base_uri' => 'http://php.net/manual/en/' ]);
+        $autoBlacklistPrefix = '';
+
+        $progress = (count($whitelisted) + count($blacklisted)) / count($allFunctions) * 100;
+        $this->say($progress.'% of functions handled, let\'s do the rest ...');
 
         foreach ($allFunctions as $f) {
             // Ignore if already handled
@@ -57,17 +61,27 @@ class RoboFile extends \Robo\Tasks {
             
             $this->yell($f);
 
-            // Get and display documentation
-            $crawler = new \Symfony\Component\DomCrawler\Crawler(
-                $client->get('function.'.str_replace('_', '-', $f).'.php')->getBody()->getContents()
-            );
-            $this->say($crawler->filter('.refpurpose')->text());
-            $this->say($crawler->filter('.refsect1.description')->text());
+            if ($autoBlacklistPrefix != '' && substr($f, 0, strlen($autoBlacklistPrefix)) == $autoBlacklistPrefix) {
+                $this->say('Blacklisted automatically.');
+                $blacklisted[] = $f;
+                continue;
+            }
+            $autoBlacklistPrefix = '';
 
+            try {
+                // Get and display documentation
+                $crawler = new \Symfony\Component\DomCrawler\Crawler(
+                    $client->get('function.'.str_replace('_', '-', $f).'.php')->getBody()->getContents()
+                );
+                $this->say($crawler->filter('.refpurpose')->text());
+                $this->say($crawler->filter('.refsect1.description')->text());
+            } catch (\Exception $e) {
+                $this->say('Error while retrieving information: '.$e->getMessage());
+            }
             // Ask what to do
             $action = null;
-            while (!in_array($action, ['w','b','s','x']))
-                $action = $this->ask('(w)hitelist, (b)lacklist, (s)kip, e(x)it');
+            while (!in_array($action, ['w','b','a','s','x']))
+                $action = $this->ask('(w)hitelist, (b)lacklist, (a)ll blacklist, (s)kip, e(x)it');
             
             switch ($action) {
                 case "w":
@@ -76,11 +90,18 @@ class RoboFile extends \Robo\Tasks {
                 case "b":
                     $blacklisted[] = $f;
                     break;
+                case "a":
+                    $blacklisted[] = $f;
+                    $autoBlacklistPrefix = substr($f, 0, strpos($f, '_')+1);
+                    break;
                 case "x":
                     file_put_contents('data/function.whitelist.json', json_encode($whitelisted));
                     file_put_contents('data/function.blacklist.json', json_encode($blacklisted));
                     exit(0);
             }
+            // Save on every step to prevent data loss from crashes
+            file_put_contents('data/function.whitelist.json', json_encode($whitelisted));
+            file_put_contents('data/function.blacklist.json', json_encode($blacklisted));
         }
     }
 

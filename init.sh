1502c01f8d32478fe88fc4aa0bcfecac5c2eaf1c
Lukas Rosenstock <lr@lukasrosenstock.net>
2018-12-05 15:10:23 +0100

    Allow MODE env variable for Docker deployment

--- init.sh
+++ init.sh
@@ -1,10 +1,14 @@
+# Apply defaults for unset variables
+if [ -z $MODE ]; then MODE=default; fi
+if [ -z $CLIENT_AUTH ]; then CLIENT_AUTH=shared_secret:runclass; fi
+
 # Write configuration file
 
 echo "<?php return array(" \
 	" 'uploads' => false, " \
 	" 'cache_dir' => __DIR__.'/cache', " \
 	" 'uploads_dir' => __DIR__.'/uploads', " \
-	" 'mode' => 'hybrid', " \
+	" 'mode' => '$MODE', " \
 	" 'client_authentication' => '$CLIENT_AUTH', " \
 	" 'client_authentication_must_be_secure' => false, " \
 	" 'co.auth_ns' => '$CO_AUTH_NS', " \

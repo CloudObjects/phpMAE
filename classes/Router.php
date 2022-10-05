3b20751f4f2e8b2bcdab13bd67eb6f35fa2fa388
LukasRos <lr@lukasrosenstock.net>
2022-10-05 16:29:32 +0200

    Added missing code

--- classes/Router.php
+++ classes/Router.php
@@ -172,6 +172,7 @@ class Router {
                     } elseif ($reader->hasProperty($r, 'phpmae:redirectsToBaseURL')) {
                         // Route to redirect to an external base URL
                         $baseUrl = $reader->getFirstValueIRI($r, 'phpmae:redirectsToBaseURL');
+                        $uri = $request->getUri();
                         $targetUrl = (string)$baseUrl->resolve($uri->getPath() . ($uri->getQuery() != '' ? '?'.$uri->getQuery() : ''));
                         
                         return (new Response)->withRedirect($targetUrl);
